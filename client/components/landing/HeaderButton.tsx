7cc56f8bbb12130b62388257cf222b589659de2e
PoulavBhowmick03 <bpoulav@gmail.com>
2024-12-01 02:03:18 +0530

    link fix

--- client/components/landing/HeaderButton.tsx
+++ client/components/landing/HeaderButton.tsx
@@ -4,7 +4,7 @@ import Link from "next/link";
 export function ShimmerButtonDemo() {
     return (
         <div className="z-10 flex items-center justify-center">
-            <Link href="https://t.me/starkfinder_bot/strk00">
+            <Link href="https://t.me/starkfinder_bot/">
                 <ShimmerButton className="shadow-2xl">
 
                     <span className="whitespace-pre-wrap text-center text-sm font-medium leading-none tracking-tight text-white dark:from-white dark:to-slate-900/10 lg:text-lg">
