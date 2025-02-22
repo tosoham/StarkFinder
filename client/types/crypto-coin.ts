7377603394364598ff09913d2da6359ddf9e7bdd
StackNq <ayanfc2003148@gmail.com>
2025-02-22 21:53:24 +0530

    changed coins and their icons

--- client/types/crypto-coin.ts
+++ client/types/crypto-coin.ts
@@ -1,9 +1,9 @@
-import xrp from "../public/crypto-icons/xrp.svg";
-import algo from "../public/crypto-icons/algo.svg";
 import usdc from "../public/crypto-icons/usdc.svg";
 import usdt from "../public/crypto-icons/usdt.svg";
-import tron from "../public/crypto-icons/trx.svg";
-import bch from "../public/crypto-icons/bch.svg";
+import strk from "../public/crypto-icons/strk.svg";
+import eth from "../public/crypto-icons/ether.svg";
+import btc from "../public/crypto-icons/btc.svg";
+import brother from "../public/crypto-icons/brother.webp";
 
 export type CryptoCoin = {
   id: string;
@@ -11,10 +11,10 @@ export type CryptoCoin = {
   logo: string;
 };
 export const CoinsLogo: CryptoCoin[] = [
-  { id: "1", name: "XRP", logo: xrp },
-  { id: "2", name: "ALGO", logo: algo },
+  { id: "1", name: "STRK", logo: strk },
+  { id: "2", name: "ETH", logo: eth },
   { id: "3", name: "USDC", logo: usdc },
   { id: "4", name: "USDT", logo: usdt },
-  { id: "5", name: "TRON", logo: tron },
-  { id: "6", name: "BCH", logo: bch },
+  { id: "5", name: "WBTC", logo: btc },
+  { id: "6", name: "BROTHER", logo: brother },
 ];
