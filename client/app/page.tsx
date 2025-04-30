dfbd98ad01843d47eaf57043f3cd92b3d33f2f68
Emmanuel Doji <dojiemma@gmail.com>
2025-04-30 20:44:12 +0100

    fix: lint error in app/page.tsx

--- client/app/page.tsx
+++ client/app/page.tsx
@@ -9,9 +9,6 @@ import {
   Question,
 } from '@/components/landing/home'
 
-import FloatingSidebar from "../components/playground/floatingWindow/FloatingSidebar"; 
-
-
 // import FloatingSidebar from "../components/playground/floatingWindow/FloatingSidebar"; 
 
 
