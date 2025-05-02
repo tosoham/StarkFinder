d42cf8df3a5d70d9c1301823edcd70352e30fc70
om santoshwar <omsantoshwar02@gmail.com>
2025-05-03 00:31:59 +0530

    add the correct contracts

--- client/components/devx/contracts/Accordion.tsx
+++ client/components/devx/contracts/Accordion.tsx
@@ -57,45 +57,33 @@ const MultiSectionAccordion = () => {
 
   const isActive = (path: string): boolean => pathname === path;
 
-  const gettingStarted: NavigationItem[] = [
-    { topic: "Install Cairo", link: "/devx/contracts/getting-started/install" },
-    { topic: "Create a new project", link: "/devx/contracts/getting-started/create" },
-    { topic: "Build your project", link: "/devx/contracts/getting-started/build" },
-    { topic: "Testing", link: "/devx/contracts/getting-started/testing" },
+  const tokenContracts: NavigationItem[] = [
+    { topic: "ERC20 Token", link: "/devx/contracts/token-contracts/erc20" },
+    { topic: "Mock ERC20", link: "/devx/contracts/token-contracts/mock-erc20" },
+    { topic: "Mock ERC721", link: "/devx/contracts/token-contracts/mock-erc721" },
   ];
 
-  const components: NavigationItem[] = [
-    { topic: "Components How-To", link: "/devx/contracts/components/how-to" },
-    { topic: "Components Dependencies", link: "/devx/contracts/components/dependencies" },
-    { topic: "Storage Collisions", link: "/devx/contracts/components/storage" },
-    { topic: "Ownable", link: "/devx/contracts/components/ownable" },
+  const defiContracts: NavigationItem[] = [
+    { topic: "Constant Product AMM", link: "/devx/contracts/defi-contracts/constant-product-amm" },
+    { topic: "Concentrated Liquidity AMM", link: "/devx/contracts/defi-contracts/concentrated-liquidity-amm" },
+    { topic: "DeFi Vault", link: "/devx/contracts/defi-contracts/defi-vault" },
+    { topic: "NFT Dutch Auction", link: "/devx/contracts/defi-contracts/nft-dutch-auction" },
+    { topic: "Crowdfunding", link: "/devx/contracts/defi-contracts/crowdfunding" },
+    { topic: "Staking Contract", link: "/devx/contracts/defi-contracts/staking-contract" },
   ];
 
-  const applications: NavigationItem[] = [
-    {
-      topic: "Upgradeable Contract",
-      link: "/devx/contracts/applications/upgradeable-contract",
-    },
-    { topic: "DeFi Vault", link: "/devx/contracts/applications/defi-vault" },
-    { topic: "ERC20 Token", link: "/devx/contracts/applications/erc20-token" },
-    { topic: "ERC721 NFT", link: "/devx/contracts/applications/erc721-nft" },
+  const utilityContracts: NavigationItem[] = [
+    { topic: "Simple Storage", link: "/devx/contracts/utility-contracts/simple-storage" },
+    { topic: "Random Number", link: "/devx/contracts/utility-contracts/random-num" },
+    { topic: "Merkle", link: "/devx/contracts/utility-contracts/merkle" },
+    { topic: "Timelock", link: "/devx/contracts/utility-contracts/timelock" },
+    { topic: "Evolving NFT", link: "/devx/contracts/utility-contracts/evolving-nft" },
   ];
 
-  const advancedConcept: NavigationItem[] = [
-    { topic: "Hashing", link: "/devx/contracts/advanced-concept/hashing" },
-    { topic: "Plugins", link: "/devx/contracts/advanced-concept/plugins" },
-    {
-      topic: "Signature Verification",
-      link: "/devx/contracts/advanced-concept/verification",
-    },
-    { topic: "Sierra IR", link: "/devx/contracts/advanced-concept/sierra" },
-  ];
-
-  const cairoCheatsheet: NavigationItem[] = [
-    { topic: "Felt", link: "/devx/contracts/cairo-cheatsheet/felt" },
-    { topic: "Events", link: "/devx/contracts/cairo-cheatsheet/events" },
-    { topic: "Maps", link: "/devx/contracts/cairo-cheatsheet/maps" },
-    { topic: "Arrays", link: "/devx/contracts/cairo-cheatsheet/arrays" },
+  const governanceContracts: NavigationItem[] = [
+    { topic: "Upgradable", link: "/devx/contracts/governance-contracts/upgradable" },
+    { topic: "Stark Identity", link: "/devx/contracts/governance-contracts/starkidentity" },
+    { topic: "Starkfinder", link: "/devx/contracts/governance-contracts/starkfinder" },
   ];
 
   return (
@@ -106,7 +94,7 @@ const MultiSectionAccordion = () => {
             className="flex justify-between items-center"
             onClick={() => toggleSection("getting-started")}
           >
-            <h1 className="text-lg font-medium">Getting Started</h1>
+            <h1 className="text-lg font-medium">Token Contracts</h1>
             {openSections["getting-started"] ? (
               <ChevronUp size={20} />
             ) : (
@@ -116,7 +104,7 @@ const MultiSectionAccordion = () => {
           {openSections["getting-started"] && (
             <div className="mt-2 ml-4 text-gray-300">
               <ul className="pl-2 space-y-1">
-                {gettingStarted.map((item, index) => (
+                {tokenContracts.map((item, index) => (
                   <li key={index} className={isActive(item.link) ? "text-white font-bold" : ""}>
                     <Link href={item.link} prefetch={false}>{item.topic}</Link>
                   </li>
@@ -131,7 +119,7 @@ const MultiSectionAccordion = () => {
             className="flex justify-between items-center"
             onClick={() => toggleSection("components")}
           >
-            <h1 className="text-lg font-medium">Components</h1>
+            <h1 className="text-lg font-medium">Defi Contracts</h1>
             {openSections["components"] ? (
               <ChevronUp size={20} />
             ) : (
@@ -141,7 +129,7 @@ const MultiSectionAccordion = () => {
           {openSections["components"] && (
             <div className="mt-2 ml-4 text-gray-300">
               <ul className="pl-2 space-y-1">
-                {components.map((item, index) => (
+                {defiContracts.map((item, index) => (
                   <li key={index} className={isActive(item.link) ? "text-white font-bold" : ""}>
                     <Link href={item.link} prefetch={false}>{item.topic}</Link>
                   </li>
@@ -156,7 +144,7 @@ const MultiSectionAccordion = () => {
             className="flex justify-between items-center"
             onClick={() => toggleSection("applications")}
           >
-            <h1 className="text-lg font-medium">Applications</h1>
+            <h1 className="text-lg font-medium">Utility Contracts</h1>
             {openSections["applications"] ? (
               <ChevronUp size={20} />
             ) : (
@@ -166,7 +154,7 @@ const MultiSectionAccordion = () => {
           {openSections["applications"] && (
             <div className="mt-2 ml-4 text-gray-300">
               <ul className="pl-2 space-y-1">
-                {applications.map((item, index) => (
+                {utilityContracts.map((item, index) => (
                   <li key={index} className={isActive(item.link) ? "text-white font-bold" : ""}>
                     <Link href={item.link} prefetch={false}>{item.topic}</Link>
                   </li>
@@ -181,7 +169,7 @@ const MultiSectionAccordion = () => {
             className="flex justify-between items-center"
             onClick={() => toggleSection("advanced-concept")}
           >
-            <h1 className="text-lg font-medium">Advanced Concepts</h1>
+            <h1 className="text-lg font-medium">Governance Contracts</h1>
             {openSections["advanced-concept"] ? (
               <ChevronUp size={20} />
             ) : (
@@ -191,7 +179,7 @@ const MultiSectionAccordion = () => {
           {openSections["advanced-concept"] && (
             <div className="mt-2 ml-4 text-gray-300">
               <ul className="pl-2 space-y-1">
-                {advancedConcept.map((item, index) => (
+                {governanceContracts.map((item, index) => (
                   <li key={index} className={isActive(item.link) ? "text-white font-bold" : ""}>
                     <Link href={item.link} prefetch={false}>{item.topic}</Link>
                   </li>
@@ -201,30 +189,6 @@ const MultiSectionAccordion = () => {
           )}
         </div>
 
-        <div className="cursor-pointer pb-3 rounded-md">
-          <div
-            className="flex justify-between items-center"
-            onClick={() => toggleSection("cairo-cheatsheet")}
-          >
-            <h1 className="text-lg font-medium">Cairo Cheatsheet</h1>
-            {openSections["cairo-cheatsheet"] ? (
-              <ChevronUp size={20} />
-            ) : (
-              <ChevronDown size={20} />
-            )}
-          </div>
-          {openSections["cairo-cheatsheet"] && (
-            <div className="mt-2 ml-4 text-gray-300">
-              <ul className="pl-2 space-y-1">
-                {cairoCheatsheet.map((item, index) => (
-                  <li key={index} className={isActive(item.link) ? "text-white font-bold" : ""}>
-                    <Link href={item.link} prefetch={false}>{item.topic}</Link>
-                  </li>
-                ))}
-              </ul>
-            </div>
-          )}
-        </div>
       </div>
     </div>
   );
