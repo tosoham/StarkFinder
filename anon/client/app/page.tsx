65babb291ceb5535a2c44d223d68fe6febd9ed82
Raymond Joseph <162983930+raymondjoseph02@users.noreply.github.com>
2025-08-23 11:40:44 +0100

    feature/implement landing page nav bar (#492)
    * feature/implement landing page nav bar
    
    * fix menu animation

--- anon/client/app/page.tsx
+++ anon/client/app/page.tsx
@@ -1,3 +1,4 @@
+import { Nav } from "@/components/ui/nav";
 import Image from "next/image";
 
 export default function Home() {
@@ -23,6 +24,113 @@ export default function Home() {
           <li className="tracking-[-.01em]">
             Save and see your changes instantly.
           </li>
+          <p>
+            Lorem ipsum dolor sit amet consectetur adipisicing elit. Id, rerum?
+            Dignissimos distinctio asperiores, vel dolore odio dolorum repellat
+            provident repellendus, quia obcaecati aut, suscipit cum aliquam
+            possimus sapiente nisi? Voluptatem. Doloremque vero, error dicta
+            maxime soluta aperiam porro ex impedit eaque harum, praesentium
+            numquam atque? Veniam eaque, repellat a possimus autem eos omnis!
+            Consequuntur vel dolores praesentium et veniam. Eaque. Velit
+            veritatis, quasi harum cum itaque optio incidunt nulla quidem
+            tempora possimus nam qui perferendis animi asperiores. Distinctio
+            ipsum officia molestiae eos, facere doloribus porro quidem ullam a
+            illum. Culpa! Cumque neque iusto ipsa consequuntur distinctio labore
+            nobis deleniti, doloribus eos. Sapiente nostrum repellendus nulla
+            totam iure cum, magni quos odit cumque possimus vitae non, laborum
+            deleniti harum in commodi! Amet inventore iste consequatur qui
+            dignissimos explicabo aut illo dolore, vitae corporis veniam nulla
+            laudantium ut a sunt, voluptatem molestiae libero, asperiores fuga
+            adipisci odio ex iure nobis. Sed, quibusdam. Reprehenderit dolores
+            unde suscipit distinctio sequi at earum sint sed similique
+            consequatur! Dolores voluptatum sapiente perferendis ipsam ab quam
+            eius, deleniti cum sed, ullam accusantium eaque similique dolor
+            dignissimos quisquam. Impedit aliquam alias dolores harum, expedita
+            eos enim illo laudantium maxime nemo animi minus similique quasi
+            quos magnam odit sequi id quibusdam fugit error molestiae atque
+            omnis? Nisi, voluptates minus. Recusandae, esse aliquid. Perferendis
+            eligendi impedit molestias laboriosam repellendus mollitia aut
+            reiciendis tenetur magnam minima qui voluptatem quae saepe nobis,
+            sunt, iure vitae. Unde quisquam debitis nemo ea reprehenderit
+            consectetur! Cum unde fuga incidunt nulla iusto impedit quidem
+            repellat dolorem alias veritatis deleniti ullam exercitationem
+            nesciunt quasi nisi modi sapiente, tempora atque facere sunt
+            suscipit. Necessitatibus iusto mollitia quidem aliquid. Iure cum
+            quasi mollitia earum voluptatibus similique laudantium vero veniam
+            reprehenderit distinctio eos qui numquam nihil, facilis, corporis
+            suscipit quia. Accusamus, harum. Modi est consectetur quisquam
+            voluptate rem, esse impedit. Tempore consectetur quam perferendis,
+            sequi, numquam quae incidunt sed tempora quisquam, cum alias. Quas
+            non perferendis quasi sapiente veniam animi quod amet eos, id vel,
+            repellendus officiis vitae asperiores commodi! Vero quasi
+            consequatur nihil exercitationem repudiandae impedit molestiae
+            perspiciatis corrupti facere omnis id magni optio maxime quos labore
+            sint, fuga ullam assumenda, voluptatum officiis aliquam ab rem
+            praesentium saepe! Fuga? Architecto aperiam dicta dolor? Porro autem
+            asperiores impedit, sunt voluptatibus suscipit, optio tempore quam
+            laudantium iure vel dignissimos, maiores commodi dolorem error?
+            Dolorum corporis, similique magni incidunt ut ea quos? Adipisci ea
+            nulla modi iusto dolores. Distinctio non asperiores enim libero
+            atque error officiis ullam dolores, pariatur blanditiis ratione
+            tempore, ducimus dolorum quo dolor aliquid similique. Sunt ad quia
+            velit? Omnis asperiores aliquid quidem molestias repellat,
+            blanditiis dolorum voluptatem dolores rerum quia beatae nesciunt
+            sint. Quos, sed! Aliquam reiciendis quo ea odio ad illo magnam
+            soluta officia, enim, natus iusto. Dolorum eligendi corporis
+            repellat saepe perferendis magni culpa assumenda, eius ipsam nostrum
+            autem quod error totam quis porro, fugit velit quo ea? Dolores
+            dolor, minus voluptate quis quaerat reiciendis eos? Suscipit
+            perspiciatis repellendus aperiam autem! Maxime consequuntur, nulla
+            rerum est ratione dolorum excepturi quisquam quod optio eligendi
+            consequatur non numquam facilis facere voluptatibus laborum velit
+            sapiente reiciendis. Soluta, iusto amet. Aliquid eveniet reiciendis
+            nam illum debitis atque nihil mollitia, odio commodi nisi, explicabo
+            qui nemo iure. Architecto obcaecati voluptatem, iure adipisci
+            blanditiis fugiat facere tempore, repellat aspernatur suscipit
+            molestias quibusdam. Velit mollitia quae vel corrupti cupiditate?
+            Nobis quas tempora nemo, modi in itaque similique corrupti magnam
+            ullam iure voluptas totam. Rem iusto quae minima, aperiam natus ea
+            laudantium nostrum dolor. Blanditiis voluptatum tempore delectus
+            ipsa harum ab eos, iure, soluta consequatur reprehenderit quasi quas
+            deserunt dolore, minus ea porro possimus voluptas laborum vero
+            magnam asperiores unde facilis! Eius, ad maiores? Omnis perspiciatis
+            quod eum! Non reprehenderit magni architecto, veritatis, cum culpa
+            vel expedita exercitationem itaque illo dolorum aliquam vitae rem,
+            eius libero! Voluptates ea exercitationem pariatur. Ea dolorem
+            reprehenderit rerum. Et consectetur repellendus impedit, tenetur
+            nemo ex ut est aut blanditiis voluptates modi qui quaerat optio,
+            iure ab omnis laboriosam ipsum consequuntur! Reprehenderit, atque!
+            Odio eveniet animi placeat porro quidem? Modi, accusamus quibusdam
+            obcaecati vel maiores optio ipsum, maxime temporibus odit
+            voluptatibus nesciunt dolorum corrupti quaerat eveniet debitis
+            expedita accusantium? Repudiandae cupiditate impedit debitis nostrum
+            commodi quidem recusandae veritatis nesciunt. Doloribus expedita,
+            magni dicta nobis itaque error corrupti assumenda libero nam
+            adipisci nostrum quasi, obcaecati tempora vero praesentium quisquam.
+            Velit voluptatibus maxime laborum saepe, pariatur accusamus quam
+            ullam rerum adipisci? Laborum, deserunt. Libero blanditiis esse
+            aliquid officia sequi minus a aperiam, enim dolor? Numquam voluptas
+            dolore nam aspernatur nihil quasi, fugiat quia a laborum quaerat?
+            Explicabo laboriosam nobis corrupti cumque! Provident deleniti
+            ratione recusandae velit eligendi dolorum ab nobis mollitia debitis
+            error, nemo similique! Repellendus alias aliquam distinctio
+            inventore quam porro hic, quasi vel asperiores eveniet earum rem
+            possimus aliquid. Quo, tempora. Labore quisquam quam sapiente id
+            facere facilis sit laborum quasi sed vero, dignissimos recusandae,
+            et aut sunt. Rem, aliquid cupiditate! Mollitia, beatae adipisci!
+            Temporibus, in? Dolorum, nulla quasi! Esse voluptatum quasi
+            dignissimos aperiam tenetur molestiae, vero laudantium corporis
+            tempore, laboriosam, qui odit sequi ipsum. Perferendis inventore
+            delectus quo vel eum quae, voluptates corporis accusantium ut.
+            Pariatur, consequatur eaque. Molestias officiis tempore maiores
+            corrupti vel, totam doloribus distinctio dolores dolore voluptas hic
+            fugiat adipisci, officia, ab sapiente sit dolor iusto minima!
+            Deleniti error fuga earum quidem voluptates ex molestias. Quos,
+            impedit! Exercitationem blanditiis excepturi omnis minus eveniet
+            voluptate praesentium? Quos expedita, cumque reiciendis odio
+            quibusdam non! Atque exercitationem ex eius deleniti. Aliquid, rem
+            illo itaque blanditiis sunt velit sequi?
+          </p>
         </ol>
 
         <div className="flex gap-4 items-center flex-col sm:flex-row">
---
+++ anon/client/components/ui/nav.tsx
@@ -0,0 +1,166 @@
+"use client";
+
+import Link from "next/link";
+import { usePathname } from "next/navigation";
+import { AnimatePresence, motion } from "framer-motion";
+import { useEffect, useState } from "react";
+import { ArrowRight, ArrowRightIcon, Moon, Sun } from "lucide-react";
+import { Button } from "@/components/ui/button";
+import { div } from "framer-motion/client";
+
+const links = [
+  { href: "/", label: "Home" },
+  { href: "/companies", label: "Companies" },
+  { href: "/about", label: "About" },
+  { href: "/docs", label: "Docs" },
+];
+
+export const Nav = () => {
+  const pathname = usePathname();
+  const [theme, setTheme] = useState<"light" | "dark">("light");
+  const [isMenuOpen, setIsMenuOpen] = useState(false);
+
+  // Load theme from localStorage
+  useEffect(() => {
+    const savedTheme = localStorage.getItem("theme") as "light" | "dark" | null;
+    if (savedTheme) {
+      setTheme(savedTheme);
+      document.documentElement.classList.toggle("dark", savedTheme === "dark");
+    }
+  }, []);
+
+  // Toggle theme
+  const toggleTheme = () => {
+    const newTheme = theme === "light" ? "dark" : "light";
+    setTheme(newTheme);
+    localStorage.setItem("theme", newTheme);
+    document.documentElement.classList.toggle("dark", newTheme === "dark");
+  };
+
+  return (
+    <header className="w-full sticky top-0 lg:pt-5 mx-auto  z-50 max-w-7xl">
+      <nav className="flex justify-between px-8 py-3 items-center max-w-7xl mx-auto bg-white lg:rounded-md dark:bg-bl">
+        {/* Logo */}
+        <div>
+          <h2
+            aria-label="anon logo"
+            className="font-bold text-xl tracking-tight dark:text-black text-black"
+          >
+            anon
+          </h2>
+        </div>
+
+        {/* Navigation Links */}
+        <ul className="hidden md:flex gap-8">
+          {links.map(({ href, label }) => {
+            const isActive = pathname === href;
+
+            return (
+              <li key={href} className="relative">
+                <Link
+                  href={href}
+                  className="font-medium text-gray-700 dark:text-gray-600 hover:text-black dark:hover:text-white transition-colors"
+                >
+                  {label}
+                </Link>
+
+                {isActive && (
+                  <motion.div
+                    layoutId="underline"
+                    className="h-0.5 bg-black dark:bg-gray-600 rounded-full absolute left-0 right-0 bottom-0 mx-auto w-full"
+                  />
+                )}
+              </li>
+            );
+          })}
+        </ul>
+
+        {/* Actions (Login + Theme Toggle) */}
+        <div className="flex items-center gap-3">
+          <Button className="px-5 py-2 rounded-full bg-black  text-white font-medium hover:bg-gray-900 transition dark:bg-black dark:text-white dark:hover:bg-gray-200">
+            Login
+          </Button>
+
+          {/* Dark Mode Toggle */}
+          <button
+            onClick={toggleTheme}
+            className="p-2 rounded-full bg-gray-100 dark:bg-gray-700 hover:bg-gray-200 dark:hover:bg-gray-600 transition hidden md:inline-flex"
+          >
+            {theme === "light" ? <Moon size={18} /> : <Sun size={18} />}
+          </button>
+
+          {/* Mobile Menu Button  */}
+          <button
+            role="menu"
+            aria-label="menu button"
+            className="flex flex-col md:hidden gap-1  rounded-md hover:bg-gray-100 dark:hover:bg-gray-700 transition  w-6"
+            onClick={() => setIsMenuOpen(!isMenuOpen)}
+          >
+            <motion.div
+              initial={{ rotate: 0, y: 0 }}
+              animate={isMenuOpen ? { rotate: 45, y: 8 } : { rotate: 0, y: 0 }}
+              transition={{ duration: 0.2 }}
+              className=" w-full h-0.5 bg-gray-900 origin-center"
+            />
+            <motion.div
+              initial={{ opacity: 1 }}
+              animate={isMenuOpen ? { opacity: 0 } : { opacity: 1 }}
+              transition={{ duration: 0.2 }}
+              className=" w-full h-0.5 bg-gray-900 "
+            />
+            <motion.div
+              initial={{ rotate: 0, y: 0 }}
+              animate={
+                isMenuOpen ? { rotate: -45, y: -8 } : { rotate: 0, y: 0 }
+              }
+              transition={{ duration: 0.2 }}
+              className=" w-full h-0.5 bg-gray-900 origin-center"
+            />
+          </button>
+
+          <AnimatePresence>
+            {/* Mobile Menu */}
+            {isMenuOpen && (
+              <motion.div
+                initial={{ opacity: 0, y: "100vh" }}
+                animate={{ opacity: 1, y: 0 }}
+                exit={{ opacity: 0, y: "-100vh" }}
+                transition={{ type: "spring", stiffness: 300, damping: 30 }}
+                className="absolute top-14 pt-8 pb-8 w-screen px-8 h-svh left-0 bg-white dark:bg-gray-800 shadow-lg  p-4 flex flex-col  gap-8 md:hidden"
+              >
+                <div className="relative">
+                  <button
+                    onClick={toggleTheme}
+                    className="p-2 rounded-full bg-gray-100 dark:bg-gray-700 hover:bg-gray-200 dark:hover:bg-gray-600 transition self-start "
+                  >
+                    {theme === "light" ? <Moon size={18} /> : <Sun size={18} />}
+                  </button>
+                </div>
+                <ul className="space-y-8">
+                  {links.map(({ href, label }) => (
+                    <li
+                      key={href}
+                      className="relative border rounded-xs border-gray-300 dark:border-gray-700 pb-2 h-10"
+                    >
+                      <Link
+                        href={href}
+                        className="font-medium text-gray-700 dark:text-gray-300 hover:text-black dark:hover:text-white transition-colors bg-white pb-2 dark:bg-gray-800 absolute w-[102%] -left-1 top-3  z-10"
+                      >
+                        <div className="flex justify-between items-center w-full px-3">
+                          <span>{label}</span>
+                          <span>
+                            <ArrowRightIcon className="font-extralight text-gray-400 " />
+                          </span>
+                        </div>
+                      </Link>
+                    </li>
+                  ))}
+                </ul>
+              </motion.div>
+            )}
+          </AnimatePresence>
+        </div>
+      </nav>
+    </header>
+  );
+};
