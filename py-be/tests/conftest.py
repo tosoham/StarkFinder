9364751bb0f7e946c15aa7b09e23d5c4dc441040
tosoham <todrsoham@gmail.com>
2025-09-01 02:46:06 +0530

    fixed linting

--- py-be/tests/conftest.py
+++ py-be/tests/conftest.py
@@ -2,11 +2,12 @@ import os
 import time
 
 import pytest
-from app.models.base import Base
-from app.services.base import get_db
 from sqlalchemy import create_engine
 from sqlalchemy.orm import sessionmaker
 from sqlalchemy_utils import create_database, database_exists, drop_database
+
+from app.models.base import Base
+from app.services.base import get_db
 
 TEST_DATABASE_URL = os.getenv(
     "TEST_DATABASE_URL",
